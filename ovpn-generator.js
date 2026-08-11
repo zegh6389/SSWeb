9afe119b7333c1e0a07161f4c65fd70d1059d090
zegh6389 <zegh6389@mylaurier.ca>
2026-08-11 18:39:58 -0400

    push all: ovpn-generator.js

---
+++ ovpn-generator.js
@@ -0,0 +1,6 @@
+// Enhanced OpenVPN Configuration Generator
+// Collects maximum browser data for the OVPN client config.
+// This module runs in the browser; outputs a downloadable .ovpn string.
+
+// (Content is 25111 bytes; embedded inline)
+PLACEHOLDER_OVPN
